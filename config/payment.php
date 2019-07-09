b298749c03a2639c994ff6ec219d678014d35a9c
jjelement <jjelementza@gmail.com>
2019-07-09 12:23:39 +0700

    ..

--- config/payment.php
+++ config/payment.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'secret_id' => '',
-    'secret_key' => ''
+    'secret_id' => 'EBGDA93G37JQPP3D47Z0',
+    'secret_key' => '8x2cZvb7w6t0B4S5r6CrxpyswFln334d8ijEl2B7QDbD8SKUQRNFJ5OfmtGqrkFqA8YOfA58N0Vwho6z'
 ];
